e783d649367a4390ebac1d4d3f35f666acb604b5
Michael Fairchild <mfairchild365@gmail.com>
2017-01-05 10:49:38 -0600

    Implement tests for the import user form

--- src/Tests/ImportUserTest.php
+++ src/Tests/ImportUserTest.php
@@ -3,7 +3,6 @@
 namespace Drupal\unl_user\Tests;
 
 use Drupal\simpletest\WebTestBase;
-use Drupal\unl_user\PersonDataQuery;
 
 /**
  * Tests for the unl_user module.
@@ -25,7 +24,7 @@ class ImportUserTest extends WebTestBase
   public function setUp() {
     parent::setUp();
     $this->user = $this->drupalCreateUser(array(
-      'administer site configuration'
+      'administer users'
     ));
   }
 
@@ -35,7 +34,21 @@ class ImportUserTest extends WebTestBase
   public function testImportUser() {
     //We need an admin to log in...
     $this->drupalLogin($this->user);
-    
-    $this->assertTrue(true);
+
+    //Make sure we can go to the user import form
+    $this->drupalGet('/admin/people/import');
+    $this->assertResponse(200);
+
+    $this->drupalPostForm(NULL, array(
+      'search' => 'test'
+    ), t('Search'));
+    $this->assertText('Records Found. Select a user to import.', 'Able to search for a user');
+
+    $this->drupalPostForm(NULL, array(
+      'uid' => 'test-user2'
+    ), t('Import Selected User'));
+    $this->assertText('imported test-user2', 'able to import a user');
+
+    $this->assertTrue(preg_match('/user\/(\d)\/edit/', $this->getUrl()), 'Should take you to the edit page for the new user');
   }
 }
